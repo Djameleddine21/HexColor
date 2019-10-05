d07b68db2dc2bccb180ffe4bdb43da110dd6d9e3
Djameleddine21 <hd_sebbagh@esi.dz>
2019-10-05 15:52:53 +0100

    add the colorpicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp( MyApp());
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+void main() {
+  runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hex Color"),centerTitle: true,),
+        body:
+             ColorPicker(
+              pickerColor: Color(0xffcc00),
+              onColorChanged: null,
+              pickerAreaHeightPercent: 0.8 ,
+              colorPickerWidth: 300,
+              enableLabel: true,
+            )
+      ),
+    );
+  }
+}
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HexColor',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text("Hex Color"),primary: true,centerTitle: true,),
-        body: Center(child: Text("Hello world"),),
-      ) 
-    );
+        title: 'HexColor',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Hex Color"),
+              primary: true,
+              centerTitle: true,
+            ),
+            body: ColorPicker(color: Colors.blue, onChanged: (value) {})
+      )
+    )
+            ;
   }
-}
+}*/
